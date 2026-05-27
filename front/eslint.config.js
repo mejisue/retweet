bdf5224daee9655267eb90e1279b29df222f333e
jisoo <februstar11@gmail.com>
2026-05-27 12:56:16 +0900

    fix: eslint rules를 rules 블록 안으로 이동

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -19,7 +19,9 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-explicit-any": "off",
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
 ]);
